897bdba4c7f1d79d49c55ee0c9c1cc7b4db3b104
Deckason <chibuikemchidera@gmail.com>
2024-09-26 13:40:46 +0100

    Used the company logo as favIcon and also added page titles for SEO optimization to home, About us, Services, Login and signup pages as they are the only available pages now

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,11 @@ import Updates from './components/landingPage/updates/Updates'
 import FrequentAskedQuestions from './components/landingPage/frequentlyAskedQuestions/FrequentAskedQuestions'
 import ContactUs from './components/landingPage/contactUS/ContactUs'
 
+export const metadata = {
+  title: "JamorTech - Empowering New Tech Talent Through Internships",
+  description: "Empower Your Potential with JamorTech - Join us on a transformative journey where aspiring tech professionals gain the skills and knowledge needed to thrive in today’s competitive landscape."
+};
+
 const page = () => {
   return (
     <main >
