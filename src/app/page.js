caffbde07eb996d0f4dafb5106971c7ab90d8c5a
Jamor-Technology <jamortechnology@gmail.com>
2024-10-09 08:52:28 +0000

    I deckason made some changes to ogechukwus work, the changes are on the layout file and the page.js file in the app folder. Reason being that ogechukwus files were behind compared to what have been pushed online and there were a few changes she made to these files which would automatically make changes effective to these files thereby repklacing these changes with the ones erarlier made.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,41 @@
+import React from 'react'
+import Hero from './components/landingPage/heroSection/Hero'
+import OurServices from './components/landingPage/ourServices/OurServices'
+import Benefits from './components/landingPage/benefits/Benefits'
+import AboutUS from './components/landingPage/aboutUsSection/AboutUS'
+import OurPartners from './components/landingPage/OurPartnersSection/OurPartners'
+import Updates from './components/landingPage/updates/Updates'
+import FrequentAskedQuestions from './components/landingPage/frequentlyAskedQuestions/FrequentAskedQuestions'
+import ContactUs from './components/landingPage/contactUS/ContactUs'
 
+export const metadata = {
+  title: "JamorTech - Empowering New Tech Talent Through Internships",
+  description: "Empower Your Potential with JamorTech - Join us on a transformative journey where aspiring tech professionals gain the skills and knowledge needed to thrive in today’s competitive landscape.",
+  openGraph: {
+    title: "Welcome to JamorTech - Your Pathway to Tech Careers",
+    description: "Join JamorTech to kickstart your tech career with our internship programs.",
+    type: "website",
+    locale: "en_Us",
+    url: "https://jamorweb.netlify.app/",
+    siteName: "JamorTech"
+  },
+  twitter: { 
+    card: "summary_large_image",
+  },
+};
 
 const page = () => {
   return (
-    <div>
-      <h1>Welcome home</h1>
-    </div>
+    <main >
+      <Hero />
+      <OurServices />
+      <Benefits />
+      <AboutUS />
+      <OurPartners />
+      <Updates />
+      <FrequentAskedQuestions />
+      <ContactUs />
+    </main>
   )
 }
 
