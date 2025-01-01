00880dd06f2fdc0ac78fa450f5e500d61ac5b04d
Deckason <chibuikemchidera@gmail.com>
2025-01-01 10:47:10 +0100

    changed the error message being passed for login form which was displaying password must be atleast 6 characters instead of displaying atleast 8 characters

--- src/app/(pages)/(auth)/login/page.jsx
+++ src/app/(pages)/(auth)/login/page.jsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
     if (!formData.password) {
       newErrors.password = 'Password is required'
     } else if (formData.password.length < 8) {
-      newErrors.password = 'Password must be at least 6 characters'
+      newErrors.password = 'Password must be at least 8 characters'
     }
     
     setErrors(newErrors)
