c1ef42741cf068a7d3096f55641ccc3867622f15
deckason <chibuikemchidera@gmail.com>
2025-02-11 14:15:47 -0800

    Updated the next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['https://res.cloudinary.com', "res.cloudinary.com"]
-    }
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
